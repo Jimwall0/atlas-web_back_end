db8d0c5fa7dcefd4cd777be0aabc6b8574a2e538
Tsimmuaj <tyang4204@gmail.com>
2025-03-10 16:53:15 -0500

    added return

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,8 +2,7 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
+    return [task, task2];
   }
   return [task, task2];
 }
