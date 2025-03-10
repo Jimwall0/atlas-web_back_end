acdfb82f325f6c372a3266b8b9caa36b3228d016
Tsimmuaj <tyang4204@gmail.com>
2025-03-10 17:21:14 -0500

    fixed syntax

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  return myarray = [...array1, ...array2, ...string];
+  const myArray = [...array1, ...array1, ...string];
+  return myArray;
 }
