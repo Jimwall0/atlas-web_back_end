c096b72f935196c654a48ae9dd5b39d3a7b64dbc
Tsimmuaj <tyang4204@gmail.com>
2025-03-10 17:53:12 -0500

    budget object

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+  return budget;
+}
