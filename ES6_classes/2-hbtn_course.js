b73eaf05cac257351cea9f935e825d5ed4545baf
Tsimmuaj <tyang4204@gmail.com>
2025-03-11 13:58:00 -0500

    update

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,16 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = length;
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
     this._students = students;
   }
 
@@ -18,14 +27,23 @@ export default class HolbertonCourse {
   }
 
   set name(string) {
+    if (typeof string !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = string;
   }
 
   set length(string) {
+    if (typeof string !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = string;
   }
 
   set students(string) {
+    if (!Array.isArray(string)) {
+      throw new TypeError('Students must be an array');
+    }
     this._students = string;
   }
 }
