4c145f50355bfe4a5977fdc0c6fc7c16496bc0f9
Tsimmuaj <tyang4204@gmail.com>
2025-03-11 13:22:35 -0500

    correct output

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -5,9 +5,9 @@ const initializeRooms = () => {
   const one = new ClassRoom(19);
   const two = new ClassRoom(20);
   const three = new ClassRoom(34);
-  list[0] = one._maxStudentsSize;
-  list[1] = two._maxStudentsSize;
-  list[2] = three._maxStudentsSize;
+  list[0] = one;
+  list[1] = two;
+  list[2] = three;
   return list;
 };
 
