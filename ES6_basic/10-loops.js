80f191bad5cde12f6a33c91af98b742f74a9acef
Tsimmuaj <tyang4204@gmail.com>
2025-03-10 20:26:34 -0500

    assign to a array

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const temp = {};
   for (const idx of array) {
     const value = array[idx];
-    temp[idx] = appendString + value;
+    temp += `${appendString}-${value}`
   }
   return temp;
 }
