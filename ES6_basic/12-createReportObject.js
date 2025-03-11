2fd5cf8b457f5534ec6868adab180025fc080736
Tsimmuaj <tyang4204@gmail.com>
2025-03-10 22:35:43 -0500

    return object keys

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,11 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
     getNumberOfDepartments(object) {
-      let num = 0;
-      for (const _ of Object.keys(object)) {
-        num += 1;
-      }
-      return num;
+      return Object.keys(object).length;
     },
   };
 }
