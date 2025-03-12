e242adb7c0d443e1a58f35555e3104fde53f1f69
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 15:56:46 -0500

    fixed concat error

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+const cleanSet = (set, startString) => {
+  const setArray = [...set];
+  let string = '';
+  for (let i = 0; i < setArray.length; i += 1) {
+    const x = setArray[i];
+    if (x.startsWith(startString) && startString !== '') {
+      if (i === 0) {
+        string = x.slice(3);
+      } else {
+        string += '-'.concat(x.slice(3));
+      }
+    }
+  }
+  return string;
+};
+
+export default cleanSet;
