9dd653766c14b29ea55df7da243d5eeed2a40696
Tsimmuaj <tyang4204@gmail.com>
2025-03-11 22:19:41 -0500

    semi colon

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -5,7 +5,7 @@ export default class Building {
     }
     this._sqft = sqft;
     if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage')
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
