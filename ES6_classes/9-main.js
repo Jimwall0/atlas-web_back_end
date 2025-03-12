df996192a5d45eef056a1ff196234d3a22773c6d
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 01:52:03 -0500

    double check

--- ES6_classes/9-main.js
+++ ES6_classes/9-main.js
@@ -1,6 +1,8 @@
+import { StudentHolberton } from "./9-hoisting";
 import listOfStudents from "./9-hoisting.js";
 
 console.log(listOfStudents);
+console.log(listOfStudents[0] instanceof StudentHolberton);
 
 const listPrinted = listOfStudents.map(
     student => student.fullStudentDescription
