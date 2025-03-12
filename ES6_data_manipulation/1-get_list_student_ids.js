a614407be60e107a0ca1f6959dd98ce37935d6c7
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 12:38:05 -0500

    more linter

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,7 @@ const getListStudentIds = (array) => {
   if (!Array.isArray(array)) {
     return [];
   }
-  return array.map(id => (id.id));
+  return array.map((id) => id.id);
 };
 
 export default getListStudentIds;
