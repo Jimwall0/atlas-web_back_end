46b0984c12a76468e128570a475220ff7eadad57
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 12:23:17 -0500

    simple array map

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,6 @@
 const getListStudents = (array) => {
-  if (Array.isArray(array) === false) {
-    return [];
-  }
-  const map1 = new Map();
-  for (const num of array) {
-    map1.set('id', num.id);
-  }
-  return map1;
+  const myArray = array.map(temp => temp.id);
+  return myArray;
 };
 
 export default getListStudents;
