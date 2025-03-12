42f485b082dab438016b86080bfad99a4fb2f03c
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 16:30:26 -0500

    undefined check

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 const cleanSet = (set, startString) => {
-  if (startString === undefined) {
-    startString = '';
+  let check = startString;
+  if (check === undefined) {
+    check = '';
   }
   const setArray = [...set];
   let string = '';
   for (let i = 0; i < setArray.length; i += 1) {
     const x = setArray[i];
-    if (x.startsWith(startString) && startString !== '') {
+    if (x.startsWith(check) && check !== '') {
       if (i === 0) {
         string = x.slice(3);
       } else {
