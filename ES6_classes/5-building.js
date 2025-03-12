9b60d410e94549193e8d41a3c93d4beab3b0312f
Tsimmuaj <tyang4204@gmail.com>
2025-03-11 22:26:37 -0500

    Linter

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -5,8 +5,8 @@ export default class Building {
     }
     this._sqft = sqft;
     if (
-      this.constructor !== Building &&
-      this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+      this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
     ) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
