f6d07d23767603a767e69d3298485635ded1e873
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 00:03:44 -0500

    save

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name needs to be a string');
+    }
+    this._name = name;
+    if (typeof code !== 'string') {
+      throw new TypeError('Code needs to be a string');
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name needs to be a string');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code needs to be a string');
+    }
+    this._code = value;
+  }
+
+  get toString() {
+    return `[object ${this.code}]`;
+  }
+}
