cd759d972f816f23402fba1ded5e75c82862071f
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 17:31:02 -0500

    correct output

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 const updateUniqueItems = (groceriesList) => {
   for (const [key, value] of groceriesList) {
     if (value === 1) {
-      groceriesList.set[key, 100];
+      groceriesList.set(key, 100);
     }
   }
 }
