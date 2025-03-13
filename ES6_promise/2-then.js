af6a62b3a284ea0af0f3f9f54b6b42bf4c860fa2
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 19:16:25 -0500

    another task 2 test

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,3 +1,15 @@
-const handleResponseFromAPI = (promise) => promise.then(() => console.log('Got a response from the API'));
+const handleResponseFromAPI = (promise) => {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  });
+  promise.catch(() => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
+}
 
 export default handleResponseFromAPI;
