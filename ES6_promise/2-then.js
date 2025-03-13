64cd8b573ded689617339cfc180a286b0971d23b
Tsimmuaj <tyang4204@gmail.com>
2025-03-12 19:44:26 -0500

    check

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,15 +1,19 @@
 const handleResponseFromAPI = (promise) => {
-  promise.then((success) => {
-    console.log('Got a response from the API');
-    resolve({
-      status: 200,
-      body: success,
-    });
+  const temp = new Promise((resolve, reject) => {
+    promise
+      .then((success) => {
+        console.log('Got a response from the API');
+        resolve({
+          status: 200,
+          body: success,
+        });
+      })
+      .catch(() => {
+        console.log('Got a response from the API');
+        reject(new Error());
+      });
   });
-  promise.catch(() => {
-    console.log('Got a response from the API');
-    reject(new Error());
-  });
+  return temp;
 };
 
 export default handleResponseFromAPI;
