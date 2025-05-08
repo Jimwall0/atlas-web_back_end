f97e717ab7690ff1a032194daafab7a1246b6b5d
Tsimmuaj <tyang4204@gmail.com>
2025-05-08 14:24:37 -0500

    formating

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,5 +2,7 @@
 """
 Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
 """
+
+
 def add(num1: float, num2: float) -> float:
     return num1 + num2
