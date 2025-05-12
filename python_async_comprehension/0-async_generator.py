1e904174c0004723ce991ebc2a901d96c66d6f48
Tsimmuaj <tyang4204@gmail.com>
2025-05-11 20:29:44 -0500

    save

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,10 +10,10 @@ import asyncio
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     returns 10 numbers over ten seconds
     """
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
