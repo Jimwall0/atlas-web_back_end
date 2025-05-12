634c3520ac123494f77454b39b676917112b7b1a
Tsimmuaj <tyang4204@gmail.com>
2025-05-11 19:55:15 -0500

    outputing

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Take the code from wait_n and alter it into a
+new function task_wait_n. The code is nearly identical
+to wait_n except task_wait_random is being called.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Simply makes a corotine task
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
+
+
+async def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
+    tempList = []
+    waitList = []
+    for i in range(n):
+        tempList.append(task_wait_random(max_delay))
+    for coro in asyncio.as_completed(tempList):
+        waitList.append(await coro)
+    return waitList
