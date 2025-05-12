d9250c81df59a5cfdefa01162046dc3462bea2b5
Tsimmuaj <tyang4204@gmail.com>
2025-05-11 19:34:21 -0500

    fixed for correct function type

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -13,8 +13,8 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
-    return start - end
+    return end - start
