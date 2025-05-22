1260c358cef5972910d46dae53705997a5b19446
Tsimmuaj <tyang4204@gmail.com>
2025-05-22 17:39:38 -0500

    lifo cache

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Create a class LIFOCache that inherits from BaseCaching
+and is a caching system:
+
+You must use self.cache_data - dictionary from the
+parent class BaseCaching
+You can overload def __init__(self): but don’t forget
+to call the parent init: super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the item
+value for the key key.
+If key or item is None, this method should not do anything.
+If the number of items in self.cache_data is higher
+that BaseCaching.MAX_ITEMS:
+you must discard the last item put in cache (LIFO algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Cacheing using LIFO
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_list = []
+        for key in self.cache_data:
+            self.cache_list.insert(0, key)
+
+    def put(self, key, item):
+        """
+        Adds elements by using LIFO
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            self.cache_list.insert(0, key)
+            if len(self.cache_list) > self.MAX_ITEMS:
+                old_key = self.cache_list.pop(0)
+                self.cache_data.pop(old_key)
+                print(f"DISCARD: {key}")
+
+    def get(self, key):
+        """
+        Retrieves the value by key
+        """
+        if key is not None:
+            if key in self.cache_data:
+                return self.cache_data[key]
+        return None
