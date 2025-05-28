d6eb4836ad2e88bb0d965c9b78458b34ad6cfe35
Tsimmuaj <tyang4204@gmail.com>
2025-05-28 02:11:40 -0500

    changed line

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -36,7 +36,11 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper_index(
+            self,
+            index: int = None,
+            page_size: int = 10
+        ) -> Dict[str, Any]:
         """
         Implement a get_hyper_index method with two integer arguments:
         index with a None default value and page_size with default
