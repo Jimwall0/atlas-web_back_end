5ebaf995e4c0f2b301c8f7c4e0d6c780613fa3ca
Tsimmuaj <tyang4204@gmail.com>
2025-05-28 01:29:43 -0500

    working output

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -44,8 +44,6 @@ class Server:
         return self.__dataset
     
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
         dictionary = {}
         dataset = self.dataset()
         dictionary["page_size"] = len(dataset)
