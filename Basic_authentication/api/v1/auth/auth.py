33498229c7a0a4df43769572b5d07e5bb9fb6cd0
Tsimmuaj <tyang4204@gmail.com>
2025-05-29 18:02:18 -0500

    docs

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,11 +7,19 @@ from typing import List, TypeVar
 
 
 class Auth():
+    """
+    Auth class
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Requires an auth
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """Authorization a header"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user"""
         return None
