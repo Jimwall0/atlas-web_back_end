9d586012493c8090ddae6b104e357e9767062650
Tsimmuaj <tyang4204@gmail.com>
2025-05-28 21:23:39 -0500

    moved 403 outside of tuple

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -34,7 +34,7 @@ def forbidden(error) -> str:
     """
     Forbidden User
     """
-    return jsonify({"error": "Forbidden"}, 403)
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
