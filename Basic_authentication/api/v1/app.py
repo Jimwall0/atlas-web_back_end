b8e6b21026476461684fa03821a1d2700eee7377
Tsimmuaj <tyang4204@gmail.com>
2025-05-29 18:57:18 -0500

    save

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,16 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = os.getenv("AUTH_TYPE")
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+excluded = [
+    '/api/v1/status/',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/'
+]
 
 
 @app.errorhandler(404)
@@ -35,6 +45,21 @@ def forbidden(error) -> str:
     Forbidden User
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_filter():
+    """Runs before the request filter"""
+    if auth is None:
+        return
+    if not auth.require_auth(request.path, excluded):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
