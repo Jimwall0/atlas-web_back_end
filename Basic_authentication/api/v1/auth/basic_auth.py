c7b6d2ac71baf4897aac04db129a9a7ae133e30a
Tsimmuaj <tyang4204@gmail.com>
2025-05-29 19:13:57 -0500

    extract function

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,22 @@
 BasicAuth module
 """
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """
     BasicAuth class - inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+    ) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not re.search(r"(Basic\s)", authorization_header):
+            return None
+        return authorization_header[6:]
+        
