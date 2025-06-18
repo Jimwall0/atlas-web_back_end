f24facfe462d952d4a4c7b75d9d04846e353f246
Tsimmuaj <tyang4204@gmail.com>
2025-06-18 17:54:39 -0500

    pylance stuff

--- 0x0B_redix_basic/exercise.py
+++ 0x0B_redix_basic/exercise.py
@@ -8,11 +8,17 @@ from typing import Union
 
 
 class Cache:
+    """
+    Class setup to use Redis
+    """
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Sets up a key value paring and returns the key
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
