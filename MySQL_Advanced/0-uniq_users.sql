928dccab40ee8e426f2713298cc5253d4c35a96b
Tsimmuaj <tyang4204@gmail.com>
2025-06-18 19:06:34 -0500

    mysql brained

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Hey this is a table that gets your name and email
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
