6575b8fe842bb895d34545f31b086ab2fcb04b7d
Tsimmuaj <tyang4204@gmail.com>
2025-06-18 19:15:06 -0500

    CHANGED syntax

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Makes another table that also includes country
 CREATE TABLE IF NOT EXISTS users (
-    id IS NOT NULL INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     EMAIL VARCHAR(255) NOT NULL UNIQUE,
-    name, VARCHAR(255),
+    name VARCHAR(255),
     country, VARCHAR(2) NOT NULL DEFAULT 'US',
     CHECK (country IN ('US', 'CO', 'TN'))
 )
