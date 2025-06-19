098414b48addd156065661a818df1955679fd4e3
Tsimmuaj <tyang4204@gmail.com>
2025-06-18 19:05:56 -0500

    typo

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Hey this is a table that gets your name and email
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY GENERATED ALWASY AS IDENTITY,
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
