f903323efe6ad32c6831927f31860c9566c4d940
Tsimmuaj <tyang4204@gmail.com>
2025-06-18 21:23:47 -0500

    save

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- make div mathmatics
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
