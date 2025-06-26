413547efd1ce2cbe8150a97d7c3e5f4caebc30ef
Tsimmuaj <tyang4204@gmail.com>
2025-06-26 09:26:11 -0500

    correct output

--- Node_JS/1-stdin.js
+++ Node_JS/1-stdin.js
@@ -5,8 +5,10 @@ const rline = readline.createInterface({
 })
 const ask = (question) => {
     rline.question(question, (answer) => {
-        rline.write(`Your name is: ${answer}`);
-        ask(question)
+        rline.write(`Your name is: ${answer}\n`);
+        console.log("This important software is now closing");
+        rline.close();
+        process.exit(0);
     })
 }
 ask("Welcome to Holberton School, what is your name?\n");
