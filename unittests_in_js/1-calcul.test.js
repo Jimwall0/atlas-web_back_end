ec61888572ff02caf150146c79b34cdbcae57511
Tsimmuaj <tyang4204@gmail.com>
2025-07-03 22:32:53 -0500

    moved files

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('../0-calcul');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('should round and sum two whole numbers', function () {
