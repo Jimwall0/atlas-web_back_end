c23cdf2dde017f7f9bc00fee55f76a999de20b52
Tsimmuaj <tyang4204@gmail.com>
2025-07-03 23:15:15 -0500

    fixed name

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./5-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function(){
     it('should call Utils.calculateNumber with SUM, 100, 20', function(){
