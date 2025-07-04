ae044dd9f9da4ad785f697ded79f0f66c25d0016
Tsimmuaj <tyang4204@gmail.com>
2025-07-04 08:03:49 -0500

    save

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,20 +1,24 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('5-payment');
+const sendPaymentRequestToApi = require('/5-payment');
 
 describe('sendPaymentRequestToApi', function(){
-    it('will call sendPaymentRequestToAPI with 100 and 20', function(){
-        const logSpy = sinon.spy(console, 'log');
+    let logSpy;
+    beforeEach(function(){
+        logSpy = sinon.spy(console, 'log');
+    });
+    afterEach(function(){
+        logSpy.restore();
+    });
+    it('will call sendPaymentRequestToAPI with 100 and 20', function () {
         sendPaymentRequestToApi(100, 20);
         sinon.assert.calledOnce(logSpy);
         sinon.assert.calledWithExactly(logSpy, 'The total is: 120');
-        logSpy.restore();
     });
-    it('will call sendPaymentRequestToAPI with 10 and 10', function(){
-        const logSpy = sinon.spy(console, 'log');
+
+    it('will call sendPaymentRequestToAPI with 10 and 10', function () {
         sendPaymentRequestToApi(10, 10);
         sinon.assert.calledOnce(logSpy);
         sinon.assert.calledWithExactly(logSpy, 'The total is: 20');
-        logSpy.restore();
-    });
+  });
 });
