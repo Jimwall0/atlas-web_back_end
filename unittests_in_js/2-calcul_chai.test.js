8e399164f72ef6744879de79c53b4ce80bcbb201
Tsimmuaj <tyang4204@gmail.com>
2025-07-03 22:54:13 -0500

    fix

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -5,22 +5,22 @@ describe('calculateNumber', function () {
 
   describe('SUM', function(){
     it('should add two rounded numbers', function () {
-      expect(calculateNumber('SUM', 1.4, 4.5), 6); // 1 + 5
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6); // 1 + 5
     });
   });
   
   describe('SUBTRACT', function(){
     it('should subtract two rounded numbers', function () {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4); // 1 - 5
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4); // 1 - 5
     });
   });
 
   describe('DIVIDE', function(){
     it('should divide two rounded numbers', function(){
-      expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2); // 1 / 5
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2); // 1 / 5
     });
     it('should return error when divde by 0', function(){
-      expect(calculateNumber('DIVIDE', 1, 0), 'Error'); // Error
+      expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error'); // Error
     });
   });
 });
