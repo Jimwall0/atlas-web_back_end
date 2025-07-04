855d60c65ed8a38c500208ea2c000a8e12ba5c4c
Tsimmuaj <tyang4204@gmail.com>
2025-07-04 07:49:54 -0500

    stub

---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', function(){
+    it('should call Utils.calculateNumber with SUM, 100, 20', function(){
+        const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const logSpy = sinon.spy(console, 'log');
+        sendPaymentRequestToApi(100, 20);
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWith(stub, 'SUM', 100, 20);
+        sinon.assert.calledOnce(logSpy);
+        sinon.assert.calledWith(logSpy, 'The total is: 10');
+        stub.restore();
+        logSpy.restore();
+    });
+});
