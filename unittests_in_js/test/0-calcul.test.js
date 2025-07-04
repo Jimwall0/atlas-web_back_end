8720c0d26487bab7e39f1893aa26553e01a6a315
Tsimmuaj <tyang4204@gmail.com>
2025-07-03 22:28:57 -0500

    save

---
+++ unittests_in_js/test/0-calcul.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const calculateNumber = require('../0-calcul');
+
+describe('calculateNumber', function () {
+  it('should round and sum two whole numbers', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+});
