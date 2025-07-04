99077a4dc4dfa6f3f7b7be3d0a8fc69739d043a9
Tsimmuaj <tyang4204@gmail.com>
2025-07-03 22:41:46 -0500

    subtract

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -2,9 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function () {
+
   describe('SUM', function(){
     it('should add two rounded numbers', function () {
       assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6); // 1 + 5
     });
   });
+  
+  describe('SUB', function(){
+    it('should subtract two rounded numbers', function () {
+      assert.strictEqual(calculateNumber('SUB', 1.4, 4.5), -4) // 1 - 5
+    })
+  })
 });
