7c8694c7936884841870d23c853d5dfb8eb192dc
Tsimmuaj <tyang4204@gmail.com>
2025-07-10 16:46:35 -0500

    save

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,16 +1,37 @@
+const request = require('request');
 const { expect } = require('chai');
 const app = require('./api'); // your Express app
 
+let server;
+
+before((done) => {
+  server = app.listen(7865, done);
+});
+
+after((done) => {
+  server.close(done);
+})
+
 describe('Index page', function () {
-  it('should return status 200', async function () {
-    expect(response.status).to.equal(200);
+  it('should return status 200', function (done) {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('should return the correct message', async function () {
-    expect(response.text).to.equal('Welcome to the payment system');
+  it('should return the correct message', function (done) {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 
-  it('should have content-type text/html or text/plain', async function () {
-    expect(response.headers['content-type']).to.match(/text\/html|text\/plain/);
+  it('should have content-type text/html or text/plain', function (done) {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(response.headers['content-type']).to.match(/text\/html|text\/plain/);
+      done();
+    });
   });
 });
+
