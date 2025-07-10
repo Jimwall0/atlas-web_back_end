5c71375c624418c2a0106731bee7aca614663f44
Tsimmuaj <tyang4204@gmail.com>
2025-07-10 18:13:57 -0500

    fixed done error

--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -66,7 +66,7 @@ describe('Payment Page', function () {
       method: 'GET',
       json: true,
     }
-    request.get(payments, function (done) {
+    request.get(payments, function (error, response, body) {
       expect(response.statusCode).to.equal(200);
       expect(body).to.deep.equal({
         payment_method: {
