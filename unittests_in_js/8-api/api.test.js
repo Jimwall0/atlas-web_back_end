83499e7d756c383d86f000aedd88c6f9bb4d5122
Tsimmuaj <tyang4204@gmail.com>
2025-07-10 16:59:20 -0500

    test

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,37 +1,27 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api'); // your Express app
-
-let server;
-
-before((done) => {
-  server = app.listen(7865, done);
-});
-
-after((done) => {
-  server.close(done);
-})
+const app = require('./api'); // Express app
+const baseURL = 'http://localhost:7865'; // Adjust port if needed
 
 describe('Index page', function () {
   it('should return status 200', function (done) {
-    request.get('http://localhost:7865', function (error, response, body) {
+    request.get(`${baseURL}/`, function (error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should return the correct message', function (done) {
-    request.get('http://localhost:7865', function (error, response, body) {
+    request.get(`${baseURL}/`, function (error, response, body) {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
   it('should have content-type text/html or text/plain', function (done) {
-    request.get('http://localhost:7865', function (error, response, body) {
+    request.get(`${baseURL}/`, function (error, response, body) {
       expect(response.headers['content-type']).to.match(/text\/html|text\/plain/);
       done();
     });
   });
 });
-
